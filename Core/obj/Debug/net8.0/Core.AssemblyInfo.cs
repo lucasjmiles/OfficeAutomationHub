2ef7b64325ab818f7ce6cdbe026271abd9986be8
Lucas Miles <lucas.j.miles@gmail.com>
2026-06-28 21:28:48 -0400

    adding dotnet packages (CommunityToolkit.Mvvn, Serilog)

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b15f1c8d8c0bb6e13ae9c8dda785e43a341316")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+341737c15bd21c4dd042e1daa3e79ae6ebed581f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
